dc1435b5f437d0cc3ffa1a95e355dc78b93a2c1a
Ana Rey <anarey@gmail.com>
2015-03-18 11:35:50 +0100

    Modulo. requestHandlers.
    Sin respuesta de los manupuladores de peticiones.

--- nodebeginner/index.js
+++ nodebeginner/index.js
@@ -1,4 +1,11 @@
 var server = require("./server2");
 var router = require("./router");
+var requestHandlers = require("./requestsHandlers");
 
-server.iniciar(router.route);
+var handle = {}
+handle["/"] = requestHandlers.iniciar;
+handle["/"] = requestHandlers.iniciar;
+handle["/iniciar"] = requestHandlers.iniciar;
+handle["/subir"] = requestHandlers.subir;
+
+server.iniciar(router.route, handle);
